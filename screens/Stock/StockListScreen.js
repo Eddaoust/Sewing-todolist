24a34a49685b2df9d19d6f679ae9dd96586ea5da
Eddaoust <edmond.daoust@gmail.com>
2021-01-02 11:10:04 +0100

    Create Add category/product modal

--- screens/Stock/StockListScreen.js
+++ screens/Stock/StockListScreen.js
@@ -1,40 +1,38 @@
 import React, {useLayoutEffect, useRef} from 'react';
-import {StyleSheet, SafeAreaView, FlatList, View, Dimensions, Text, TouchableOpacity} from 'react-native';
+import {StyleSheet, SafeAreaView, FlatList, View, Text, TouchableOpacity, Image} from 'react-native';
 import {Modalize} from 'react-native-modalize';
 import {Portal} from 'react-native-portalize';
-import {AntDesign, Fontisto, MaterialCommunityIcons} from '@expo/vector-icons';
+import {AntDesign, MaterialCommunityIcons} from '@expo/vector-icons';
 import {useDispatch} from "react-redux";
 import StockListItem from "../../components/StockListItem";
 import HeaderIcon from "../../components/HeaderIcon";
 import SearchBar from "../../components/SearchBar";
-import TidyButton from "../../components/TidyButton";
 
-const windowHeight = Dimensions.get('window').height;
 const categories = [
     {
         id: 1,
         name: 'Mercerie',
-        img: require('../../assets/cat_back_1.png')
+        img: require('../../assets/stock_1.jpg')
     }, {
         id: 2,
         name: 'Patrons',
-        img: require('../../assets/tissustest.png')
+        img: require('../../assets/stock_2.jpg')
     }, {
         id: 3,
         name: 'Tissus',
-        img: require('../../assets/cat_back_1.png')
+        img: require('../../assets/stock_3.jpg')
     }, {
         id: 4,
         name: 'Laine',
-        img: require('../../assets/tissustest.png')
+        img: require('../../assets/stock_4.jpg')
     }, {
         id: 5,
         name: 'Mercerie',
-        img: require('../../assets/cat_back_1.png')
+        img: require('../../assets/stock_1.jpg')
     }, {
         id: 6,
         name: 'Patrons',
-        img: require('../../assets/tissustest.png')
+        img: require('../../assets/stock_2.jpg')
     }
 ];
 
@@ -96,18 +94,23 @@ const StockListScreen = ({navigation}) => {
                     disableScrollIfPossible={true}
                     adjustToContentHeight={true}>
                     <View style={styles.modalBody}>
-                        <TidyButton
-                            label="S'INSCRIRE AVEC UN E-MAIL"
-                            icon={<Fontisto name="email" size={20} color="white" style={styles.panelBtnIcon} />}
-                            onPress={() => props.navigation.navigate('RegistrationScreen')}/>
-                        <View style={styles.panelBottomTextContainer}>
-                            <Text style={styles.panelBottomtext}>Déja inscrit(e)? </Text>
-                            <TouchableOpacity
-                                activeOpacity={0.6}
-                                onPress={() => props.navigation.navigate('LoginScreen')}>
-                                <Text style={styles.panelBottomLink}>S'identifier</Text>
-                            </TouchableOpacity>
-                        </View>
+                        <Text style={styles.modalTitle}>AJOUTER</Text>
+                        <TouchableOpacity
+                            style={styles.btnContainer}
+                            onPress={() => true}>
+                            <Image
+                                style={styles.modalImg}
+                                source={require('../../assets/categorie_icon.png')}/>
+                            <Text style={styles.modalText}>Catégorie</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            style={styles.btnContainer}
+                            onPress={() => true}>
+                            <Image
+                                style={styles.modalImg}
+                                source={require('../../assets/produit_icon.png')}/>
+                            <Text style={styles.modalText}>Produit</Text>
+                        </TouchableOpacity>
                     </View>
                 </Modalize>
             </Portal>
@@ -131,30 +134,31 @@ const styles = StyleSheet.create({
     modalBody: {
         padding: 16,
         paddingTop: 50,
-        alignItems: 'center'
+        marginBottom: 70
     },
-    panelBtnIcon: {
-        marginTop: -2
-    },
-    panelBtnText: {
-        fontFamily: 'poppins-semi-bold',
+    modalTitle: {
+        color: '#35433A',
+        fontFamily: 'josefin-sans',
+        textAlign: 'center',
         fontSize: 16,
-        color: 'white',
-        paddingHorizontal: 10
+        marginBottom: 30
     },
-    panelBottomTextContainer: {
+    btnContainer: {
         flexDirection: 'row',
-        justifyContent: 'center',
-        marginTop: 20,
-        marginBottom: 40
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        paddingHorizontal: 43,
+        marginVertical: 10
     },
-    panelBottomtext:{
-        color: '#BEBEBE',
-        fontFamily: 'poppins-light'
+    modalImg: {
+        width: 35,
+        height: 35,
     },
-    panelBottomLink: {
-        color: '#F4D1D1',
-        fontFamily: 'poppins-light'
+    modalText: {
+        fontFamily: 'poppins-semi-bold',
+        fontSize: 18,
+        color: '#35433A',
+        marginLeft: 20
     }
 });
 
